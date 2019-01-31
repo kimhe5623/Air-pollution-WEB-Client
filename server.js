08ef04c71ca482b42afd9b66dde74703cf96ed79
kimhe5623 <kimhe5623@naver.com>
2019-01-31 13:33:19 -0800

    proxy problem is fixed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 
@@ -8,8 +9,12 @@ const port = process.env.PORT || 4200;
 
 app.use(express.static(path.join(__dirname, '/dist/airound')));
 
+app.use('/serverapi', proxy({ target: 'http://somnium.me:8080' }));
+
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`Airound Client is Running at 4200 port`));
+
+
